38d3131936d32b3ecd940c9d1dc2dc07e7a19180
Alejandro <alejocano22@gmail.com>
2020-05-05 13:25:00 -0500

    Host name

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -77,7 +77,7 @@ public function index()
   $totalQuotes = (count(Controller::$quotes));
   $randomNumber = (rand(0,($totalQuotes-1)));
   $randomQuote = Controller::$quotes[$randomNumber];
-  return response()->json(['quote' => $randomQuote]);
+  return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
 }
 
 public function show()
